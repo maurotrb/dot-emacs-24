ce68e419ec208fdf87cd6aee3faee050d5b75944
Mauro Taraborelli <MauroTaraborelli@gmail.com>
2014-01-06 11:24:11 +0100

    init.el: theme personalization for which function.

--- init.el
+++ init.el
@@ -25,3 +25,16 @@
 (mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;; Solarized Blue for Which Function modeline
+ '(which-func ((t (:foreground "#268bd2")))))
